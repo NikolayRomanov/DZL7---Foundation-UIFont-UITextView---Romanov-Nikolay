01abef666c6a20c093dfa2669fa8e0d0fd4673dd
Macbook <macbook@nikolay-macbook.local>
2019-02-15 15:31:44 +0200

    Initial Commit

---
+++ DZL7 - Foundation, UIFont, UITextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DZL7 - Foundation, UIFont, UITextView
+//
+//  Created by Macbook on 15.02.2019.
+//  Copyright © 2019 Nikolay_Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
